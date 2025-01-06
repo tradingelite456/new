9f4f93f74923d404a13f72cc2edbc9eebd1891b8
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-06 16:33:23 -0700

    Cleanup

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,33 +1,33 @@
 dependencies {
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
-// use an integer for version numbers
+
+// Use an integer for version numbers
 version = -1
 
 
 cloudstream {
-    // All of these properties are optional, you can safely remove them
+    // All of these properties are optional, you can safely remove them.
 
     description = "Lorem ipsum"
     authors = listOf("Cloudburst")
 
     /**
-    * Status int as the following:
+    * Status int as one of the following:
     * 0: Down
     * 1: Ok
     * 2: Slow
     * 3: Beta only
-    * */
-    status = 1
+    **/
+    status = 1 // Will be 3 if unspecified
 
     tvTypes = listOf("Movie")
 
     requiresResources = true
     language = "en"
 
-    // Random cc logo I found
+    // Random CC logo I found
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
 }
 
