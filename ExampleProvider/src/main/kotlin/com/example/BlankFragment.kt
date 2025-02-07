3e2370635baf24b59d31f11196455c88c7c47c2e
myromiles <myromiles@gmail.com>
2025-02-07 23:12:17 +0100

    Update BlankFragment.kt

--- ExampleProvider/src/main/kotlin/com/example/BlankFragment.kt
+++ ExampleProvider/src/main/kotlin/com/example/BlankFragment.kt
@@ -1,88 +1,42 @@
-package com.example
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.jsoup.Jsoup
 
-import android.content.res.ColorStateList
-import android.graphics.drawable.Drawable
-import android.os.Build
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.lagradost.cloudstream3.R
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.core.content.res.ResourcesCompat
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.lagradost.cloudstream3.utils.UIHelper.colorFromAttribute
+fun main() {
+    val client = OkHttpClient()
+    val request = Request.Builder()
+        .url("https://streamcloud.my/streamcloud/")
+        .build()
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+    client.newCall(request).execute().use { response ->
+        if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BlankFragment(val plugin: TestPlugin) : BottomSheetDialogFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+        val document = Jsoup.parse(response.body?.string())
+        val movies = document.select("div.movie-item")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        for (movie in movies) {
+            val title = movie.select("h2.title").text()
+            val description = movie.select("p.description").text()
+            val imageUrl = movie.select("img.poster").attr("src")
+            val detailPageUrl = movie.select("a").attr("href")
+
+            // Detailseite abrufen, um den Videolink zu extrahieren
+            val detailRequest = Request.Builder()
+                .url(detailPageUrl)
+                .build()
+
+            client.newCall(detailRequest).execute().use { detailResponse ->
+                if (!detailResponse.isSuccessful) throw IOException("Unexpected code $detailResponse")
+
+                val detailDocument = Jsoup.parse(detailResponse.body?.string())
+                val videoUrl = detailDocument.select("a.download-button").attr("href")
+
+                // Daten speichern oder weiterverarbeiten
+                println("Titel: $title")
+                println("Beschreibung: $description")
+                println("Bild-URL: $imageUrl")
+                println("Video-URL: $videoUrl")
+            }
         }
-    }
-
-    private fun getDrawable(name: String): Drawable? {
-        val id = plugin.resources!!.getIdentifier(name, "drawable", BuildConfig.LIBRARY_PACKAGE_NAME)
-        return ResourcesCompat.getDrawable(plugin.resources!!, id, null)
-    }
-
-    private fun getString(name: String): String? {
-        val id = plugin.resources!!.getIdentifier(name, "string", BuildConfig.LIBRARY_PACKAGE_NAME)
-        return plugin.resources!!.getString(id)
-    }
-
-    private fun <T : View> View.findView(name: String): T {
-        val id = plugin.resources!!.getIdentifier(name, "id", BuildConfig.LIBRARY_PACKAGE_NAME)
-        return this.findViewById(id)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val id = plugin.resources!!.getIdentifier("fragment_blank", "layout", BuildConfig.LIBRARY_PACKAGE_NAME)
-        val layout = plugin.resources!!.getLayout(id)
-        return inflater.inflate(layout, container, false)
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val imageView = view.findView<ImageView>("imageView")
-        val imageView2 = view.findView<ImageView>("imageView2")
-        val textView = view.findView<TextView>("textView")
-        val textView2 = view.findView<TextView>("textView2")
-
-        textView.text = getString("hello_fragment")
-        textView.setTextAppearance(view.context, R.style.ResultInfoText)
-        textView2.text = view.context.resources.getText(R.string.legal_notice_text)
-
-        imageView.setImageDrawable(
-            getDrawable("ic_android_24dp")
-        )
-        imageView.imageTintList = ColorStateList.valueOf(view.context.getColor(R.color.white))
-
-        imageView2.setImageDrawable(
-            getDrawable("ic_android_24dp")
-        )
-        imageView2.imageTintList = ColorStateList.valueOf(view.context.colorFromAttribute(R.attr.white))
     }
 }
