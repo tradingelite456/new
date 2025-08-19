aa94a098d3838da5c0baef7f8f6db7c15e1ef4db
Trading elite <tradingviewelite2@gmail.com>
2025-08-19 15:21:46 +0200

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,20 +1,21 @@
-package com.example
+package com.lagradost
 
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.TvType
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://cinepulse.to/" 
+class ExampleProvider(val plugin: TestPlugin) : MainAPI() { // all providers must be an intstance of MainAPI
+    override var mainUrl = "https://example.com/" 
     override var name = "Example provider"
     override val supportedTypes = setOf(TvType.Movie)
 
-    override var lang = "fr"
+    override var lang = "en"
 
-    // Enable this when your provider has a main page
+    // enable this when your provider has a main page
     override val hasMainPage = true
 
-    // This function gets called when you search for something
+    // this function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
         return listOf<SearchResponse>()
     }
