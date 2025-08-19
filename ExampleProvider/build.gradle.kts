e5b0f63f2a9485bd3ecb138dfe6d62ee3c8a658c
Trading elite <tradingviewelite2@gmail.com>
2025-08-19 16:06:31 +0200

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,4 +1,11 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
 dependencies {
+    implementation("com.lagradost:cloudstream3-core:1.0.0")
+    implementation("org.jsoup:jsoup:1.17.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 }
@@ -7,10 +14,8 @@ dependencies {
 version = 1
 
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
-    authors = listOf("Cloudburst", "Luna712")
+    description = "Extension CinePulse pour Cloudstream 3"
+    authors = listOf("Cinemax")
 
     /**
     * Status int as one of the following:
@@ -21,16 +26,28 @@ cloudstream {
     **/
     status = 1 // Will be 3 if unspecified
 
-    tvTypes = listOf("Movie")
+    tvTypes = listOf("Movie", "TvSeries")
 
     requiresResources = true
-    language = "en"
+    language = "fr" // Changé en français pour cinepulse.to
 
     // Random CC logo I found
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
 }
 
 android {
+    namespace = "com.example"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildFeatures {
         buildConfig = true
         viewBinding = true
